f6a2bcd76b7faa76944ec59802b2eb52a2ba83c1
Mateus Bessa <mateus_b09@hotmail.com>
2021-11-08 13:35:48 -0300

    Graph creation with random weights

--- nx.py
+++ nx.py
@@ -137,6 +137,71 @@ class GraphSearch:
                       "expanded_nodes": expanded_nodes}
         return {None: None}
 
+    def uniform_cost_search(self, goal, start):
+        # minimum cost upto
+        # goal state from starting
+        global graph, cost
+
+        # create a priority queue
+        queue = []
+
+        answer = [10 ** 8 for _ in range(len(goal))]
+
+        # insert the starting index
+        queue.append([0, start])
+
+        # map to store visited node
+        visited = {}
+
+        # count
+        count = 0
+
+        # while the queue is not empty
+        while queue:
+
+            # get the top element of the
+            queue = sorted(queue)
+            p = queue[-1]
+
+            # pop the element
+            del queue[-1]
+
+            # get the original value
+            p[0] *= -1
+
+            # check if the element is part of
+            # the goal list
+            if p[1] in goal:
+
+                # get the position
+                index = goal.index(p[1])
+
+                # if a new goal is reached
+                if answer[index] == 10 ** 8:
+                    count += 1
+
+                # if the cost is less
+                answer[index] = min(answer[index], p[0])
+                # pop the element
+                del queue[-1]
+
+                queue = sorted(queue)
+                if count == len(goal):
+                    return answer
+
+            # check for the non visited nodes
+            # which are adjacent to present node
+            if p[1] not in visited:
+                for i in range(len(graph[p[1]])):
+                    # value is multiplied by -1 so that
+                    # least priority is at the top
+                    queue.append([(p[0] + cost[(p[1], graph[p[1]][i])]) * -1, graph[p[1]][i]])
+
+            # mark as visited
+            visited[p[1]] = 1
+
+        return answer
+
 
 gc = GraphCreator()
 # G = gc.create_default_graph(GraphType.dfs)
